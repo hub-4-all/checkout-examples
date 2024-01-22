4f772c39f73d9ea7ea5e5e048feda911a185482c
Lucas Toledo Pereira <toledo.ltp@gmail.com>
2024-01-22 10:09:46 -0300

    Remove token

--- vue-3/src/App.vue
+++ vue-3/src/App.vue
@@ -23,7 +23,7 @@ export default defineComponent({
     // Esta dando erro pois as props ficam disparando useEffect do component Checkout
     // Rever por que isso ocorre
     return {
-      token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJPcmdhbml6YXRpb25VVUlEIjoiYzBlYjc1ZTctOGU4Ny00ODZlLTg4ZTUtZGIyZjIyMWZjYjgwIiwiYWNjb3VudFVVSUQiOiI5ODBiMTYzNi1jMzQ3LTQxNTctYmI1OS03MGZjMGJkMTkzZTgiLCJ1bmlxdWVJZGVudGlmaWVyIjoibHVjYXMtMDA3IiwicGF5bWVudFByb2R1Y3RzIjpbXSwiZGVzY3JpcHRpb24iOiJQYWdhbWVudG8gZGUgdW0gZXhlbXBsbyBkYSBkZW1vIGRlIDIwLTExIiwiY3VycmVuY3kiOiJFVVIiLCJhbW91bnQiOjExMTM3LCJpYXQiOjE3MDU0MTIyODF9.dGPUkOxyD8jlWKrt4P-roYgoagcsjD3LA6MY6FEX12uYFvfgU0hJggAQEm6939tmTMyE23z2-kbax18MnZ65ce2foRkW5gmwcN8B-yjBwaxVW4pqRO5Emob3gcyNOvkFzO-JWq-fTZkOYdMZ4GEHhQwSRyrUOemkFdlpE8PpKu4',
+      token: '<YOUR_PAYMENT_TOKEN>',
       onError: (err: unknown) => console.log(err),
     };
   },
